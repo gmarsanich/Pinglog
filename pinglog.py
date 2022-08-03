a295567cdcd6a156729ba74b78ce1d2e8af75b01
gmarsanich <giacomomarsanich@gmail.com>
2022-08-03 19:03:45 +0200

    Added CLI input, added progress bar

--- pinglog.py
+++ pinglog.py
@@ -1,13 +1,30 @@
 import os
 import time
+import argparse as ap
+from alive_progress import alive_bar
 
-target = str(input("Ping target: "))
-n = int(input("Ping how many times: "))
+# Getting terminal inputs
+
+parser = ap.ArgumentParser("Example")
+parser.add_argument("-t", dest="target", help="target of ping", type=str)
+parser.add_argument("-n", dest="n", help="number of times to ping target", type=int)
+args = parser.parse_args()
+
+pargs = vars(args)
+target = pargs["target"]
+n = pargs["n"]
+
+
+# Creating logfile name
+
 logfile = f"pinglog_{target}_{time.time()}.log"
-print(f"Pinging {target} {n} times...")
 
-os.system(f"ping -n {n} {target} > {logfile}")
-os.system(
-    f"powershell.exe New-BurntToastNotification -Text 'Done!', 'Saved to {os.getcwd()}\\{logfile}' -AppLogo img\\PowerShell_Core_6.0_icon.png"
-)
+# Running command
+
+with alive_bar(title=f"Pinging {target} {n} times") as bar:
+    os.system(f"ping -n {n} {target} > {logfile}")
+    os.system(
+        f"powershell.exe New-BurntToastNotification -Text 'Done!', 'Saved to {os.getcwd()}\\{logfile}' -AppLogo img\\PowerShell_Core_6.0_icon.png"
+    )
+    bar()
 print(f"Saved log to file {logfile} in {os.getcwd()}")
